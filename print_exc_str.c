8b6ba1dec32a287549aaf18d0c7f37f4f5ceb1cd
Joel Kudamo <kudamojoel@gmail.com>
2023-11-17 16:47:40 -0800

    print string only

---
+++ print_exc_str.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_exc_string - prints strings exclusively
+ * @args: arguments expected
+ * Return: an integer
+ */
+
+int print_exc_string(va_list args)
+{
+	char *s;
+	int i, length = 0;
+	int value;
+
+	s = va_arg(args, char *);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			length = length + 2;
+			value = s[i];
+			if (value < 16)
+			{
+				_putchar('0');
+				length++;
+			}
+			length = length + print_HEX_extra(value);
+		}
+		else
+		{
+			_putchar(s[i]);
+			length++;
+		}
+	}
+	return (length);
+}
