7bde04c0dd7acb1c7e7ab3ea4a775e78482b885f
Joel Kudamo <kudamojoel@gmail.com>
2023-11-17 16:36:13 -0800

    print pointer

---
+++ print_pointer.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_pointer - prints a hexa-decimal.
+ * @args: arguments  expected
+ * Return: a counter
+ */
+int print_pointer(va_list args)
+{
+
+	char *s = "(nil)";
+	void *p;
+	long int x;
+	int i;
+int y;
+
+	p = va_arg(args, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+
+	x = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	y = print_hex_extra(x);
+	return (y + 2);
+}
